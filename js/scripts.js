8f799fb1754e2ed48acafc3c7fe140cf1db1eaa2
Annie Shin and Nate Cottle <nate@>
2018-06-13 11:49:42 -0700

    fix issues with columns display.

--- js/scripts.js
+++ js/scripts.js
@@ -29,15 +29,14 @@ Player.prototype.hold = function() {
 Player.prototype.winner = function() {
   if(this.total >= 100) {
   alert(this.playerName + " are the winner!!!")
-}
+  }
 }
 
 $(document).ready(function(){
-
  $("#gameStartID").click(function(e){
    e.preventDefault();
-   player1 = new Player(true)
-   player2 = new Player(false)
+   player1 = new Player()
+   player2 = new Player()
    $("#playerNamesID").hide();
    $("#gameBoardID").show();
 
@@ -49,11 +48,8 @@ $(document).ready(function(){
    player1.playerName = player1Name;
    player2.playerName = player2Name
  });
-  // var player = new Player()
-  $("#gameStartID").click(function(e){
-    e.preventDefault();
-    $("#playerTwoRollBTNID")
-  });
+
+// Player One Roll and Hold Buttons
   $("#playerOneRollBTNID").click(function(){
     player1.roll = rollDice();
     console.log(player1.roll);
@@ -61,18 +57,17 @@ $(document).ready(function(){
     $("#player1RoundScore").text(" " + player1.tempscore)
   });
 
+  $("#playerOneHoldBTNID").click(function(){
+    player1.hold();
+    $("#player1Total").text(" " + player1.total)
+    $("#player1RoundScore").text(" " + player1.tempscore)
+    player1.winner();
+  });
+// Player Two Roll and Hold Buttons
   $("#playerTwoRollBTNID").click(function(){
     player2.roll = rollDice();
     player2.checkRoll();
     $("#player2RoundScore").text(" " + player2.tempscore)
-  });
-
-  $("#playerOneHoldBTNID").click(function(){
-    player1.hold();
-
-    $("#player1Total").text(" " + player1.total)
-    $("#player1RoundScore").text(" " + player1.tempscore)
-    player1.winner();
   });
 
   $("#playerTwoHoldBTNID").click(function(){
