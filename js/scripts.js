5151c6ec5c85f704bfcf07d9841aee09e9d910c5
Annie Shin and Nate Cottle <annie@>
2018-06-14 10:34:01 -0700

    Add in Reset button and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@ var player1="";
 var player2="";
 var compVSplayer1="";
 var computerPlayer="";
-
 var rollDice = function() {
   return Math.floor(Math.random() * (6 - 1) + 1)
 }
-
 function Player(turn) {
   this.roll = 0;
   this.tempscore = 0;
@@ -14,7 +12,6 @@ function Player(turn) {
   this.turn = turn;
   this.playerName;
 }
-
 Player.prototype.AIPlay = function() {
   for (i = 0; i < 4; i++) {
     this.roll = rollDice();
@@ -37,7 +34,6 @@ Player.prototype.checkRoll = function() {
     this.tempscore += this.roll
   }
 }
-
 Player.prototype.compCheckRoll = function() {
   if ( this.roll === 1) {
     computerPlayer.AIPlay();
@@ -47,23 +43,19 @@ Player.prototype.compCheckRoll = function() {
     this.tempscore += this.roll
   }
 }
-
 Player.prototype.resetGame = function() {
   this.tempscore = 0;
   this.total = 0;
 }
-
 Player.prototype.hold = function() {
   this.total += this.tempscore
   this.tempscore = 0;
 }
-
 Player.prototype.winner = function() {
   if(this.total >= 100) {
   alert(this.playerName + " is the winner!!!")
   }
 }
-
 // Start Screen One Player or Two Player Buttons
 $(document).ready(function(){
  $("#onePlayerVSCompStartID").click(function(event) {
@@ -72,14 +64,12 @@ $(document).ready(function(){
    $("#playerNamesID").hide();
    $("#twoPlayerOptionOpeningID").hide();
  });
-
  $("#playerVSplayerStartID").click(function(e) {
    e.preventDefault();
    $("#playerNamesID").show();
    $("#compVSplayerNamesID").hide();
    $("#twoPlayerOptionOpeningID").hide();
  });
-
  // Computer vs One Player Name Input Screen
   $("#compVSplayerNameStartID").click(function(event){
     event.preventDefault();
@@ -90,16 +80,13 @@ $(document).ready(function(){
     $("#compVSplayerNamesID").hide();
     $("#compVsgameBoardID").show();
     $("#compResetBtnID").show();
-
     var compVSplayer1Name = $("#compVSplayerOneID").val();
     $("#compVSplayer1Name").text(compVSplayer1Name + "'s")
     compVSplayer1.playerName = compVSplayer1Name;
     var computerPlayerName = "Computer Player";
     computerPlayer.playerName = computerPlayerName;
     $("#computerPlayerName").text(computerPlayerName + "'s")
-
   });
-
 // Two Players Name Input Screen
  $("#gameStartID").click(function(e){
    e.preventDefault();
@@ -108,16 +95,13 @@ $(document).ready(function(){
    $("#playerNamesID").hide();
    $("#gameBoardID").show();
    $("#ResetBtnID").show();
-
    var player1Name = $("#playerOne").val();
    $("#player1Name").text(player1Name + "'s")
    var player2Name = $("#playerTwo").val()
    $("#player2Name").text(player2Name + "'s")
-
    player1.playerName = player1Name;
    player2.playerName = player2Name
  });
-
 // Computer vs Player One Roll and Hold Buttons
   $("#compVSplayerOneRollBTNID").click(function(){
     compVSplayer1.roll = rollDice();
@@ -129,7 +113,6 @@ $(document).ready(function(){
     $("#computerPlayer2Total").text(" " + computerPlayer.total)
     $("#compVSplayer1RoundScore").text(" " + compVSplayer1.tempscore)
   });
-
   $("#compVSplayerOneHoldBTNID").click(function(){
     compVSplayer1.hold();
     $("#compVSplayer1Total").text(" " + compVSplayer1.total)
@@ -142,7 +125,6 @@ $(document).ready(function(){
     $("#computerPlayer2Total").text(" " + computerPlayer.total)
     $("#compVSplayerTurn").text(compVSplayer1.playerName + "'s Turn!")
   });
-
 // Player One Roll and Hold Buttons
   $("#playerOneRollBTNID").click(function(){
     player1.roll = rollDice();
@@ -152,7 +134,6 @@ $(document).ready(function(){
     player1.checkRoll();
     $("#player1RoundScore").text(" " + player1.tempscore)
   });
-
   $("#playerOneHoldBTNID").click(function(){
     player1.hold();
     $("#player1Total").text(" " + player1.total)
@@ -160,7 +141,6 @@ $(document).ready(function(){
     player1.winner();
     $("#playerTurn").text(player2.playerName + "'s Turn!")
   });
-
 // Player Two Roll and Hold Buttons
   $("#playerTwoRollBTNID").click(function(){
     player2.roll = rollDice();
@@ -170,7 +150,6 @@ $(document).ready(function(){
     player2.checkRoll();
     $("#player2RoundScore").text(" " + player2.tempscore)
   });
-
   $("#playerTwoHoldBTNID").click(function(){
     player2.hold();
     $("#player2Total").text(" " + player2.total)
@@ -178,17 +157,17 @@ $(document).ready(function(){
     player2.winner();
     $("#playerTurn").text(player1.playerName + "'s Turn!")
   });
+// Two Player Reset
   $("#ResetBtnID").click(function(){
     player1.resetGame();
     player2.resetGame();
-
     $("#playerTurn").empty();
     $("#player1Total").text(" " + player1.total)
     $("#player1RoundScore").text(" " + player1.tempscore)
     $("#player2Total").text(" " + player2.total)
     $("#player2RoundScore").text(" " + player2.tempscore)
   });
-
+// One Player Reset
   $("#compResetBtnID").click(function(){
     computerPlayer.resetGame();
     compVSplayer1.resetGame();
